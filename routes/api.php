3e08fa068c64147440308bf1f1bd4974e8bd5e2c
ahmadsyaifulakbar24 <ipulbelcram@gmail.com>
2022-05-11 11:36:40 +0700

    add param eselon api

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\API\Auth\LoginController;
 use App\Http\Controllers\API\Auth\LogoutController;
 use App\Http\Controllers\API\Auth\RegisterController;
 use App\Http\Controllers\API\Auth\UserController;
+use App\Http\Controllers\API\Params\ParamsController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -26,6 +27,11 @@ use Illuminate\Support\Facades\Route;
     Route::post('auth/login', LoginController::class);
     Route::post('auth/register', RegisterController::class);
 // end auth route
+
+// param route 
+    Route::get('param/eselon1', [ParamsController::class, 'get_eselon1']);
+    Route::get('param/eselon2', [ParamsController::class, 'get_eselon2']);
+// end param route 
 Route::middleware('auth:api')->group(function() {
     // auth route 
         Route::get('auth/user', UserController::class);
